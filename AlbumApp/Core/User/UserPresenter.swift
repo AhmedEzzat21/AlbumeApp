efc51302587df1248539cd584a59ac59f7d0a4d3
ahmed.ezzat <a7med.ibn.3zzat@gmail.com>
2020-08-23 01:59:12 +0200

    add userpresenter

--- AlbumApp/Core/User/UserPresenter.swift
+++ AlbumApp/Core/User/UserPresenter.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2020 Ahmed. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Bond
+
+class UserPresenter: BasePresenter {
+    
+    var router: RouterManagerProtocol
+    let userName: Observable<String?> = Observable("")
+    let userAdress: Observable<String?> = Observable("")
+    var albumes: Dynamic<[AlbumModel]> = Dynamic([])
+    let albumeTitle: Observable<String?> = Observable("")
+    let albumeId: Observable<String?> = Observable("")
+    let userRepo: UserRepo
+    
+    init(router: RouterManagerProtocol, userRepo: UserRepo) {
+        self.router = router
+        self.userRepo = userRepo
+        
+    }
+    
+    override func hydrate() {
+        
+    }
+    
+    
+}
