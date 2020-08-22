38c9dfc255a191b825c3cb12342b68421a389485
ahmed.ezzat <a7med.ibn.3zzat@gmail.com>
2020-08-23 00:43:06 +0200

    first commit

---
+++ AlbumApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  AlbumApp
+//
+//  Created by Ahmed on 8/23/20.
+//  Copyright © 2020 Ahmed. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
