d691e1aee2220c895aad8f649de066b3faafdd95
ahmed.ezzat <a7med.ibn.3zzat@gmail.com>
2020-08-23 02:13:23 +0200

    ad profile view

---
+++ AlbumApp/Shell/User/UserView.swift
@@ -0,0 +1,53 @@
+//
+//  UserView.swift
+//  AlbumApp
+//
+//  Created by Ahmed on 8/23/20.
+//  Copyright © 2020 Ahmed. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserView: BaseView<UserPresenter, BaseItem> {
+    
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var UserAddressLbl: UILabel!
+    
+    override func bindind() {
+        presenter = UserPresenter(router: RouterManager(self), userRepo: UserRepoImpl())
+        presenter.userName.bind(to: userNameLbl)
+        presenter.userAdress.bind(to: UserAddressLbl)
+
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        
+        if #available(iOS 11.0, *) {
+            navigationController?.navigationBar.prefersLargeTitles = true
+            navigationController?.navigationBar.topItem?.title = "Profile"
+            navigationController?.navigationItem.largeTitleDisplayMode = .automatic
+            
+            let attributes = [
+                NSAttributedString.Key.foregroundColor : UIColor.black,
+            ]
+            
+            navigationController?.navigationBar.largeTitleTextAttributes = attributes
+            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+            self.navigationController?.navigationBar.shadowImage = UIImage()
+            UINavigationBar.appearance().largeTitleTextAttributes = [
+                NSAttributedString.Key.foregroundColor: UIColor.black
+            ]
+            // Play around with the duration until you find
+            // a time interval, you find suitable
+            UIView.animate(withDuration: 2) {
+                self.navigationController?.navigationBar.isTranslucent = true
+            }
+            
+        } else {
+            // Fallback on earlier versions
+        }
+        
+    }
+    
+    
+}
