0f3de2ab53afacb5349dba8ffaa750e861ce296d
ahmed.ezzat <a7med.ibn.3zzat@gmail.com>
2020-08-23 14:43:05 +0200

    enhance in navigation

--- AlbumApp/Shell/Photos/PhotosView.swift
+++ AlbumApp/Shell/Photos/PhotosView.swift
@@ -37,8 +37,8 @@ class PhotosView: BaseView<PhotosPresenter, albumeItem> {
         
         if #available(iOS 11.0, *) {
             navigationController?.navigationBar.prefersLargeTitles = true
+            self.navigationItem.title = presenter.albumeTitle.value
             navigationController?.navigationBar.topItem?.title = "Profile"
-            
             navigationController?.navigationItem.largeTitleDisplayMode = .automatic
             
             let attributes = [
