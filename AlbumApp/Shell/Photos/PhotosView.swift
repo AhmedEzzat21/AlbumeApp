2f238967f2695c8c3b9e0dfee090178fc6050b6c
ahmed.ezzat <a7med.ibn.3zzat@gmail.com>
2020-08-23 14:51:40 +0200

    enhance

--- AlbumApp/Shell/Photos/PhotosView.swift
+++ AlbumApp/Shell/Photos/PhotosView.swift
@@ -39,7 +39,8 @@ class PhotosView: BaseView<PhotosPresenter, albumeItem> {
         
         if #available(iOS 11.0, *) {
             navigationController?.navigationBar.prefersLargeTitles = true
-            navigationController?.navigationBar.topItem?.title = "Profile"           self.navigationItem.backBarButtonItem?.title = "Profile"
+            navigationController?.navigationBar.topItem?.title = "Profile"
+            self.navigationItem.backBarButtonItem?.title = "Profile"
 
             navigationController?.navigationItem.largeTitleDisplayMode = .automatic
             
