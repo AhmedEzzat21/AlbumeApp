526e90833a0d6308fb161ab6e5cc71ec1c581af2
ahmed.ezzat <a7med.ibn.3zzat@gmail.com>
2020-08-23 14:00:34 +0200

    enhance in details

--- AlbumApp/Shell/Photos/PhotosView.swift
+++ AlbumApp/Shell/Photos/PhotosView.swift
@@ -78,13 +78,21 @@ extension PhotosView: UICollectionViewDataSource, UICollectionViewDelegate, UICo
         
     }
     
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
- 
-            return CGSize( width: 100, height: 50 )
 
-        
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        return CGSize(width: collectionViewWidth/3, height: collectionViewWidth/3)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
 }
