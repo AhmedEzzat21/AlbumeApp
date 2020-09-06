4ae995814e1d9705947f4b02e8c066626c674c88
ahmed.ezzat <a7med.ibn.3zzat@gmail.com>
2020-09-06 16:48:03 +0200

    fix

--- AlbumApp/Shell/Photos/PhotosView.swift
+++ AlbumApp/Shell/Photos/PhotosView.swift
@@ -87,7 +87,7 @@ extension PhotosView: UICollectionViewDataSource, UICollectionViewDelegate, UICo
         
     }
     
-
+   
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
